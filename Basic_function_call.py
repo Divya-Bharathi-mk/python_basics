962a54109fd615bcd277cccd59c888fa6bbbdd28
Divya Bharathi L <divyabharathi9925@gmail.com>
2026-02-19 16:32:38 +0530

    Add files via upload

---
+++ Basic_function_call.py
@@ -0,0 +1,31 @@
+def add():
+    print("Addition")
+    a=int(input("Enter number"))
+    b=int(input("Enter number"))
+    sum=a+b
+    print(sum)
+def sub():
+    print("Subtraction")
+    a=int(input("Enter number: "))
+    b=int(input("Enter number: "))
+    ded=a-b
+    print(ded)
+def mul():
+    print("Multiplication")
+    a=int(input("Enter number: "))
+    b=int(input("Enter number: "))
+    mult=a*b
+    print(mult)
+def div():
+    print("Division")
+    a=int(input("Enter number: "))
+    b=int(input("Enter number: "))
+    divi=a/b
+    print(divi)
+
+add()
+sub()
+mul()
+div()
+
+
