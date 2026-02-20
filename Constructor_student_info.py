9c04ad81161cef25c743984a06279bd4da6b9a10
Divya Bharathi L <divyabharathi9925@gmail.com>
2026-02-20 14:37:40 +0530

    Add files via upload

---
+++ Constructor_student_info.py
@@ -0,0 +1,21 @@
+class student:
+    def __init__(self,name,age,mark):
+        self.name=name
+        self.age=age
+        self.mark=mark
+
+# Creating objects
+        
+obj1=student("Divya",26,788)
+obj2=student("Sathish",26,988)
+
+# Printing details
+
+print("Name : ",obj1.name)
+print("Age : ",obj1.age)
+print("Mark : ",obj1.mark)
+
+print("Name : ",obj2.name)
+print("Age : ",obj2.age)
+print("Mark : ",obj2.mark)
+
